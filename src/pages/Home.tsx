2c01f075c949bc896938c7574a3524d0d8b5ba8b
dannyswiss1 <dannyswiss9ija@gmail.com>
2025-10-30 05:57:19 +0100

    project initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Layout, Text } from "@stellar/design-system";
+// import { GuessTheNumber } from "../components/GuessTheNumber";
+
+const Home: React.FC = () => (
+  <Layout.Content>
+    <Layout.Inset>
+      <Text as="h1" size="xl">
+        Welcome to your cylo!
+      </Text>
+      <Text as="p" size="md">
+        Your most trusted platform for farm to table delivery. Buy fresh, buy cheap🍅.
+      </Text>
+    </Layout.Inset>
+  </Layout.Content>
+);
+
+export default Home;
