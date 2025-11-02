bae52359f0505a590bf6699e61288e6bcbd88a91
blurbeast <leumasre@gmail.com>
2025-11-02 12:27:34 +0100

    contract errors moved

--- contracts/cylo-escrow/src/cylo_escrow.rs
+++ contracts/cylo-escrow/src/cylo_escrow.rs
@@ -24,18 +24,18 @@ pub enum DataKey {
     FarmerOrders, 
 }
 
-#[contracterror]
-#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-pub enum CyloEscrowErrors {
-    AlreadyInitialized = 1,
-    NotInitialized = 2,
-    InvalidOrderId = 3,
-    Unauthorized = 4,
-    InvalidInput = 5,
-    TransferFailed = 6,
-    AlreadyProcessed = 7,
-    NotExpired = 8,
-}
+// #[contracterror]
+// #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+// pub enum CyloEscrowErrors {
+//     AlreadyInitialized = 1,
+//     NotInitialized = 2,
+//     InvalidOrderId = 3,
+//     Unauthorized = 4,
+//     InvalidInput = 5,
+//     TransferFailed = 6,
+//     AlreadyProcessed = 7,
+//     NotExpired = 8,
+// }
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
---
+++ contracts/cylo-escrow/src/errors.rs
@@ -0,0 +1,14 @@
+use soroban_sdk::{contracterror};
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+pub enum CyloEscrowErrors {
+    AlreadyInitialized = 1,
+    NotInitialized = 2,
+    InvalidOrderId = 3,
+    Unauthorized = 4,
+    InvalidInput = 5,
+    TransferFailed = 6,
+    AlreadyProcessed = 7,
+    NotExpired = 8,
+}
